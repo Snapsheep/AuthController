50acbb311106c42db950efc61a66dfe745ea3c2a
Wacharakorn Promthong <wacharakorn.p@traceongroup.com>
2017-10-20 17:01:31 +0700

    All update

---
+++ bootstrap/app.php
@@ -0,0 +1,63 @@
+<?php
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'testview',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ]
+    ],
+
+]);
+
+
+$container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule) {
+    return $capsule;
+};
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
+        'cache' => false,
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+    return $view;
+
+};
+
+$container['validator'] = function ($container) {
+    return new \App\Validation\Validator;
+};
+
+$container['HomeController'] = function ($container) {
+    return new \App\Controllers\HomeController($container);
+};
+
+$container['AuthController'] = function ($container) {
+    return new \App\Controllers\Auth\AuthController($container);
+};
+
+$app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
+$app->add(new \App\Middleware\OldInputMiddleware($container));
+
+
+require __DIR__ . '/../app/routes.php';
